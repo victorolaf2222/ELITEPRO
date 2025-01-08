ba6c609a2544fa6897a56e9c024d623dfb2af34e
victorolaf2222 <victorolafusi88@gmail.com>
2025-01-08 10:06:19 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,15 +2,15 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "2348109263390"
-global.ownername = "ElitePro"
+global.ownernomer = "2347046975070"
+global.ownername = "DFW_DRÆCO"
 global.ytname = "YT: @ElitePro-Empire"
 global.socialm = "GitHub: Elite-MD"
 global.location = "Nigeria, Port Harcourt"
 
-global.ownernumber = '2348109263390'  //creator number
-global.ownername = 'ElitePro' //owner name
-global.botname = 'ELITE-PRO' //name of the bot
+global.ownernumber = '2347046975070'  //creator number
+global.ownername = 'DFW_DRÆCO' //owner name
+global.botname = 'REDDLINE99' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
